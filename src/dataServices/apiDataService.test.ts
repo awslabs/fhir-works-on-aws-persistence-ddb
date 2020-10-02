29a74c55f169ec90c9250ec3481e97e593853ccb
Tim Nguyen <nguyen102@users.noreply.github.com>
2020-10-02 09:58:51 -0400

    feat: API to communicate with Integration Transform (#21)

---
+++ src/dataServices/apiDataService.test.ts
@@ -0,0 +1,419 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import {
+    CreateResourceRequest,
+    DeleteResourceRequest,
+    ReadResourceRequest,
+    ResourceNotFoundError,
+    UpdateResourceRequest,
+} from 'fhir-works-on-aws-interface';
+import { ApiDataService } from './apiDataService';
+
+const resource = {
+    resourceType: 'Patient',
+    text: {
+        status: 'generated',
+        div: '<div xmlns="http://www.w3.org/1999/xhtml"><p></p></div>',
+    },
+    active: true,
+    name: [
+        {
+            family: 'Smith',
+            given: ['Emily'],
+        },
+    ],
+    gender: 'female',
+    birthDate: '1995-09-24',
+    managingOrganization: {
+        reference: 'Organization/2.16.840.1.113883.19.5',
+        display: 'Good Health Clinic',
+    },
+    meta: {
+        versionId: '1',
+        lastUpdated: '2020-09-30T14:17:52.867Z',
+    },
+    id: 'f2ddf33c-9344-49cd-991f-8273eb959f92',
+};
+
+const apiDataService = new ApiDataService('http://localhost:4000');
+const mock = new MockAdapter(axios);
+afterEach(() => {
+    mock.reset();
+});
+expect.hasAssertions();
+
+const url = `http://localhost:4000/persistence/Patient`;
+describe('CREATE', () => {
+    test('create resource successfully', async () => {
+        // BUILD
+        const data = {
+            message: '',
+            resource,
+        };
+
+        mock.onPost(url).reply(201, data);
+
+        const createResourceRequest: CreateResourceRequest = {
+            resourceType: 'Patient',
+            resource: '',
+        };
+
+        // OPERATE
+        const response = await apiDataService.createResource(createResourceRequest);
+
+        // CHECK
+        expect(response.resource).toEqual(resource);
+    });
+
+    test('api returns with 400 error', async () => {
+        // BUILD
+        const message = 'Failed to parse request';
+        const data = {
+            message,
+            resource: {},
+        };
+        mock.onPost(url).reply(400, data);
+
+        const createResourceRequest: CreateResourceRequest = {
+            resourceType: 'Patient',
+            resource: '',
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.createResource(createResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error(`An error was received from the Integration Transform: ${message}`));
+        }
+    });
+
+    test('api returns with 500 error', async () => {
+        // BUILD
+        const message = 'Failed to parse request';
+        const data = {
+            message,
+            resource: {},
+        };
+        mock.onPost(url).reply(500, data);
+
+        const createResourceRequest: CreateResourceRequest = {
+            resourceType: 'Patient',
+            resource: '',
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.createResource(createResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error(`An error was received from the Integration Transform: ${message}`));
+        }
+    });
+
+    test('network error', async () => {
+        // BUILD
+        mock.onPost(url).networkError();
+
+        const createResourceRequest: CreateResourceRequest = {
+            resourceType: 'Patient',
+            resource: '',
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.createResource(createResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error('Failed to connect to the Integration Transform'));
+        }
+    });
+});
+
+describe('READ', () => {
+    test('read resource successfully', async () => {
+        // BUILD
+        const data = {
+            message: '',
+            resource,
+        };
+
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onGet(`${url}/${id}`).reply(200, data);
+
+        const readResourceRequest: ReadResourceRequest = {
+            id,
+            resourceType: 'Patient',
+        };
+
+        // OPERATE
+        const response = await apiDataService.readResource(readResourceRequest);
+        // CHECK
+        expect(response.resource).toEqual(resource);
+    });
+
+    test('api returns with 404', async () => {
+        // BUILD
+        const message = 'Failed to find resource';
+        const data = {
+            message,
+            resource: {},
+        };
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onGet(`${url}/${id}`).reply(404, data);
+
+        const resourceType = 'Patient';
+        const readResourceRequest: ReadResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.readResource(readResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof ResourceNotFoundError).toBeTruthy();
+            expect(e).toMatchObject(new ResourceNotFoundError(resourceType, id, message));
+        }
+    });
+
+    test('api returns with 500', async () => {
+        // BUILD
+        const message = 'Failed to find resource';
+        const data = {
+            message,
+            resource: {},
+        };
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onGet(`${url}/${id}`).reply(500, data);
+
+        const resourceType = 'Patient';
+        const readResourceRequest: ReadResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.readResource(readResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error(`An error was received from the Integration Transform: ${message}`));
+        }
+    });
+
+    test('network error', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onGet(`${url}/${id}`).networkError();
+
+        const resourceType = 'Patient';
+        const readResourceRequest: ReadResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.readResource(readResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error('Failed to connect to the Integration Transform'));
+        }
+    });
+});
+
+describe('UPDATE', () => {
+    test('update resource successfully', async () => {
+        // BUILD
+        const data = {
+            message: '',
+            resource,
+        };
+
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onPut(`${url}/${id}`).reply(200, data);
+
+        const updateResourceRequest: UpdateResourceRequest = {
+            id,
+            resourceType: 'Patient',
+            resource,
+        };
+
+        // OPERATE
+        const response = await apiDataService.updateResource(updateResourceRequest);
+        // CHECK
+        expect(response.resource).toEqual(resource);
+    });
+
+    test('api returns with 404 error', async () => {
+        // BUILD
+        const message = 'Failed to parse request';
+        const data = {
+            message,
+            resource: {},
+        };
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onPut(`${url}/${id}`).reply(404, data);
+
+        const resourceType = 'Patient';
+        const updateResourceRequest: UpdateResourceRequest = {
+            id,
+            resourceType,
+            resource,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.updateResource(updateResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof ResourceNotFoundError).toBeTruthy();
+            expect(e).toMatchObject(new ResourceNotFoundError(resourceType, id, message));
+        }
+    });
+
+    test('api returns with 500 error', async () => {
+        // BUILD
+        const message = 'Failed to parse request';
+        const data = {
+            message,
+            resource: {},
+        };
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onPut(`${url}/${id}`).reply(500, data);
+
+        const resourceType = 'Patient';
+        const updateResourceRequest: UpdateResourceRequest = {
+            id,
+            resourceType,
+            resource,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.updateResource(updateResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error(`An error was received from the Integration Transform: ${message}`));
+        }
+    });
+
+    test('network error', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onPut(`${url}/${id}`).networkError();
+
+        const resourceType = 'Patient';
+        const updateResourceRequest: UpdateResourceRequest = {
+            id,
+            resourceType,
+            resource,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.updateResource(updateResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error('Failed to connect to the Integration Transform'));
+        }
+    });
+});
+
+describe('DELETE', () => {
+    test('delete resource successfully', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onDelete(`${url}/${id}`).reply(204);
+
+        const deleteResourceRequest: DeleteResourceRequest = {
+            id,
+            resourceType: 'Patient',
+        };
+
+        // OPERATE
+        const response = await apiDataService.deleteResource(deleteResourceRequest);
+        // CHECK
+        expect(response).toEqual({ message: '' });
+    });
+
+    test('api returns with 404 error', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        const message = 'Failed to find resource';
+        const data = {
+            message,
+        };
+        mock.onDelete(`${url}/${id}`).reply(404, data);
+
+        const resourceType = 'Patient';
+        const deleteResourceRequest: DeleteResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.deleteResource(deleteResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof ResourceNotFoundError).toBeTruthy();
+            expect(e).toMatchObject(new ResourceNotFoundError(resourceType, id, message));
+        }
+    });
+
+    test('api returns with 500 error', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        const message = 'Failed to find resource';
+        const data = {
+            message,
+        };
+        mock.onDelete(`${url}/${id}`).reply(500, data);
+
+        const resourceType = 'Patient';
+        const deleteResourceRequest: DeleteResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.deleteResource(deleteResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error(`An error was received from the Integration Transform: ${message}`));
+        }
+    });
+
+    test('network error', async () => {
+        // BUILD
+        const id = 'f2ddf33c-9344-49cd-991f-8273eb959f92';
+        mock.onDelete(`${url}/${id}`).networkError();
+
+        const resourceType = 'Patient';
+        const deleteResourceRequest: DeleteResourceRequest = {
+            id,
+            resourceType,
+        };
+
+        try {
+            // OPERATE
+            await apiDataService.deleteResource(deleteResourceRequest);
+        } catch (e) {
+            // CHECK
+            expect(e instanceof Error).toBeTruthy();
+            expect(e).toMatchObject(new Error('Failed to connect to the Integration Transform'));
+        }
+    });
+});
---
+++ src/dataServices/apiDataService.ts
@@ -0,0 +1,119 @@
+/* eslint-disable class-methods-use-this */
+
+import {
+    ConditionalDeleteResourceRequest,
+    CreateResourceRequest,
+    DeleteResourceRequest,
+    GenericResponse,
+    PatchResourceRequest,
+    Persistence,
+    ReadResourceRequest,
+    ResourceNotFoundError,
+    UpdateResourceRequest,
+    vReadResourceRequest,
+} from 'fhir-works-on-aws-interface';
+import axios from 'axios';
+
+export class ApiDataService implements Persistence {
+    updateCreateSupported: boolean = false;
+
+    private INTEGRATION_TRANSFORM_URL: string;
+
+    constructor(integrationTransformUrl: string) {
+        this.INTEGRATION_TRANSFORM_URL = integrationTransformUrl;
+    }
+
+    async createResource(request: CreateResourceRequest): Promise<GenericResponse> {
+        try {
+            const url = `${this.INTEGRATION_TRANSFORM_URL}/persistence/${request.resourceType}`;
+            const response = await axios.post(url, request.resource);
+            return { message: '', resource: response.data.resource };
+        } catch (e) {
+            throw this.getError(e);
+        }
+    }
+
+    async readResource(request: ReadResourceRequest): Promise<GenericResponse> {
+        try {
+            const response = await axios.get(
+                `${this.INTEGRATION_TRANSFORM_URL}/persistence/${request.resourceType}/${request.id}`,
+            );
+            return { message: '', resource: response.data.resource };
+        } catch (e) {
+            throw this.getError(e, request.resourceType, request.id);
+        }
+    }
+
+    async updateResource(request: UpdateResourceRequest): Promise<GenericResponse> {
+        try {
+            const response = await axios.put(
+                `${this.INTEGRATION_TRANSFORM_URL}/persistence/${request.resourceType}/${request.id}`,
+                request.resource,
+            );
+            return { message: '', resource: response.data.resource };
+        } catch (e) {
+            throw this.getError(e, request.resourceType, request.id);
+        }
+    }
+
+    async deleteResource(request: DeleteResourceRequest): Promise<GenericResponse> {
+        try {
+            await axios.delete(`${this.INTEGRATION_TRANSFORM_URL}/persistence/${request.resourceType}/${request.id}`);
+            // Don't need to actually return anything to the router
+            return { message: '' };
+        } catch (e) {
+            return this.getError(e, request.resourceType, request.id);
+        }
+    }
+
+    getError(e: any, resourceType: string = '', id: string = '') {
+        if (e.response) {
+            const message = e.response.data.message || '';
+            const statusCode = e.response.status || undefined;
+            if (id !== '' && statusCode === 404) {
+                return new ResourceNotFoundError(resourceType, id, message);
+            }
+            console.error('An error was received from the Integration Transform', {
+                message,
+                statusCode,
+            });
+            return new Error(`An error was received from the Integration Transform: ${message}`);
+        }
+        return new Error('Failed to connect to the Integration Transform');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async deleteVersionedResource(resourceType: string, id: string, vid: number) {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async vReadResource(request: vReadResourceRequest): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    conditionalCreateResource(request: CreateResourceRequest, queryParams: any): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    conditionalUpdateResource(request: UpdateResourceRequest, queryParams: any): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    patchResource(request: PatchResourceRequest): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    conditionalPatchResource(request: PatchResourceRequest, queryParams: any): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    conditionalDeleteResource(request: ConditionalDeleteResourceRequest, queryParams: any): Promise<GenericResponse> {
+        throw new Error('Method not implemented.');
+    }
+}
