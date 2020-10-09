af5e274fe417187d750441f2b8385506db9611ae
Nestor Carvantes <carvantes@gmail.com>
2020-10-09 12:19:52 -0700

    feat: Add startCrawler Lambda (#26)

---
+++ src/bulkExport/startCrawler.ts
@@ -0,0 +1,31 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Handler } from 'aws-lambda';
+import AWS from 'aws-sdk';
+
+export const startCrawlerHandler: Handler = async event => {
+    const { CRAWLER_NAME } = process.env;
+    if (CRAWLER_NAME === undefined) {
+        throw new Error('CRAWLER_NAME environment variable is not defined');
+    }
+    const glue = new AWS.Glue();
+    try {
+        console.log(`Starting crawler ${CRAWLER_NAME}`);
+        await glue
+            .startCrawler({
+                Name: CRAWLER_NAME,
+            })
+            .promise();
+        console.log('Crawler started successfully');
+        return event;
+    } catch (e) {
+        if (e.code === 'CrawlerRunningException') {
+            console.log('Crawler is already running');
+            return event;
+        }
+        throw e;
+    }
+};
